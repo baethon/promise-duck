b9b95365934b6d19d8d57391ffad32dffccd1901
Radoslaw Mejer <radmen@desmart.com>
2018-01-07 11:44:23 +0100

    refactor(thenable): then() default values

--- src/thenable.js
+++ src/thenable.js
@@ -1,15 +1,10 @@
 const wrapper = require('./wrapper')
 
-const defaults = {
-  resolve: value => value,
-  reject: error => Promise.reject(error)
-}
-
 module.exports = (fn, { memoize = true } = {}) => {
   const wrappedFn = memoize ? wrapper.memoize(fn) : wrapper.promiseable(fn)
 
   return {
-    then (resolve = defaults.resolve, reject = defaults.reject) {
+    then (resolve, reject = Promise.reject) {
       return wrappedFn()
         .then(resolve, reject)
     }
