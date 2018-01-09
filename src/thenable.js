880548ad5f6aac769a3107791e5c7354482d30cf
Radoslaw Mejer <radmen@desmart.com>
2018-01-09 17:23:03 +0100

    docs(thenable): add docblock to thenable()

--- src/thenable.js
+++ src/thenable.js
@@ -1,5 +1,13 @@
 const wrapper = require('./wrapper')
 
+/**
+ * Converts given function to thenable object
+ *
+ * @param  {Function} fn
+ * @param  {Object}   options
+ * @param  {Boolean}  options.memoize [true] should the result of fn be memoized
+ * @return {Object}
+ */
 module.exports = (fn, { memoize = true } = {}) => {
   const wrappedFn = memoize ? wrapper.memoize(fn) : wrapper.promiseable(fn)
 
